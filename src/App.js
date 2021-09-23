a35398c3e51f228e11f8e899dbd1379cfaf09ad4
Shahid <shahidpt0982219@gmail.com>
2021-09-23 14:10:07 +0530

    Installed fonts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Display from './components/Display/Display';
 const App = () => {
     return(
         <div>
-            <h1>Hello React !</h1>
             <Display result="8657" expression="12 X 100 + 298 / 19 - 234"/>
             <Keypad />
         </div>
--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Keypad.module.css'
+import styles from './Keypad.module.css';
 
 
 const Keypad = () => {
